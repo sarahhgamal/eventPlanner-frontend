7b4f036767a8b04e07d5e45f2ca9779d080888a3
mariamnassar-1 <nassarmariam07@gmail.com>
2025-11-22 14:02:27 +0200

    add auth gaurd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Auth } from './components/auth/auth';
 import { EventList } from './components/event-list/event-list';
+import { AuthGuard, NoAuthGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', component: Auth },
-  { path: 'event-list', component: EventList }
+  { path: 'auth', component: Auth, /*canActivate: [NoAuthGuard]*/ },
+  { path: 'event-list', component: EventList, /*canActivate: [AuthGuard]*/ } // temp disaable auth guard for ui testing
 ];
--- src/app/components/event-list/event-list.ts
+++ src/app/components/event-list/event-list.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth';
 
 @Component({
   selector: 'app-event-list',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './event-list.css',
 })
 export class EventList {
+  constructor(private auth: AuthService, private router: Router) {}
 
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/auth']);
+  }
 }
